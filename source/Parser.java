4e84449eb8ed2ce98dc28cb84a24b847f3655040
Arthur <b_arthp@outlook.com>
2021-09-25 22:42:24 -0300

    Basic classes and octave example in Nice.java

---
+++ source/Parser.java
@@ -0,0 +1,21 @@
+package source;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Parser {
+    private Tokens[] notes, commands;
+
+    public Tokens getNotesByID(int id){
+        if (id >= notes.length){
+            id = notes.length-1;
+        }
+        return this.notes[id];
+    }
+    public Tokens getCommandsByID(int id){
+        if (id >= commands.length){
+            id = commands.length-1;
+        }
+        return this.commands[id];
+    }
+}
