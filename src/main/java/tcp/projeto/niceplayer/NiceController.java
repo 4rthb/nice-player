b84997a8cdb80451b502749988ebe825e831cdd9
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2021-11-15 22:33:30 -0300

    add file handling

--- src/main/java/tcp/projeto/niceplayer/NiceController.java
+++ src/main/java/tcp/projeto/niceplayer/NiceController.java
@@ -1,10 +1,13 @@
 package tcp.projeto.niceplayer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
@@ -13,6 +16,10 @@ import org.jfugue.player.ManagedPlayerListener;
 import org.jfugue.player.Player;
 
 import javax.sound.midi.Sequence;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Optional;
 
 public class NiceController {
     private Player player = new Player();
@@ -91,17 +98,51 @@ public class NiceController {
 
     @FXML
     protected void onNew() {
+        if(userInput.getLength() != 0) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmação de novo arquivo");
+            alert.setHeaderText("Confirma criar uma nova música?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if(result.get() == ButtonType.CANCEL) {
+                return;
+            }
+        }
     }
 
     @FXML
     protected void onOpen() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Abrir nova música");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Arquivos de texto bruto", "*.txt"),
+                new FileChooser.ExtensionFilter("Arquivos Nice", "*.nice")
+        );
+        File selectedFile = fileChooser.showOpenDialog(userInput.getScene().getWindow());
+
+        try {
+            userInput.setText((new FileReader(selectedFile)).toString());
+        } catch(FileNotFoundException e) {
+
+        };
     }
 
     @FXML
     protected void onSaveMIDI() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Salvar MIDI");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Arquivo MIDI", "*.mid")
+        );
+        File selectedFile = fileChooser.showSaveDialog(userInput.getScene().getWindow());
     }
 
     @FXML
     protected void onSaveTXT() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Salvar TXT");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Arquivo TXT", "*.txt")
+        );
+        File selectedFile = fileChooser.showSaveDialog(userInput.getScene().getWindow());
     }
 }
